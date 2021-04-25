6e40aa6e68473da3cefbe4bb9013c68b446bdd27
Alaa Eddine <keni91cell@gmail.com>
2021-04-25 15:33:16 +0200

    adds logic to change the count color

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -6,10 +6,21 @@ import "./styles.scss";
 
 const Counter = ({ title }) => {
   const count = useSelector((state) => state.count);
+
   return (
     <div className="counter_container">
       <h1>{title}</h1>
-      <h2 className={count % 2 === 0 ? "white_color" : "red_color"}>{count}</h2>
+      <h2
+        style={
+          count === 0
+            ? { color: "white" }
+            : count % 2 === 0
+            ? { color: "black" }
+            : { color: "red" }
+        }
+      >
+        {count}
+      </h2>
       <div className="buttons">
         <DecreaseButton />
         <IncreaseButton />
